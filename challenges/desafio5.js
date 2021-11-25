14e92a5d014fc20058db2c7166453d734f2c0c68
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2021-11-25 10:24:55 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  { $and: [
+    { ingredientes: { $nin: ["ketchup"] } },
+    { nome: { $ne: "McChicken" } },
+    ],
+  },
+  { $push: {
+    ingredientes: "ketchup",
+    },
+  },
+  { upsert: true },
+);
+
+db.produtos.find({},
+  { nome: true, ingredientes: true, _id: false },
+ );
