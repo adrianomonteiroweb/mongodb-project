0ad5f3a3b6eb187e198b0fa9f3c6afb3264c3cdc
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2021-11-25 11:10:30 -0300

    lint desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,6 +4,7 @@ db.produtos.updateMany({}, { $set: { vendasPorDia: semanaArray } }, {});
 
 db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } }, {});
 
-db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } }, {});
+db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } },
+{ $inc: { "vendasPorDia.6": 120 } }, {});
 
 db.produtos.find({}, { nome: true, vendasPorDia: true, _id: false });
